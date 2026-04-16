c271afee220b2062ec77bd21a00aeb698f5eafdc
tezehcalvin-blip <tezehcalvin@gmail.com>
2026-04-16 02:51:57 +0100

    config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    OWNER_NUMBER: "237XXXXXXXXX@s.whatsapp.net",
+
+    AI_KEYS: {
+        openai: "YOUR_OPENAI_KEY",
+        gemini: "YOUR_GEMINI_KEY",
+        grok: "YOUR_GROK_KEY"
+    }
+};
